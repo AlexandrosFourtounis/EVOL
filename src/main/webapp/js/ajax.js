661fc5acc00aee9991d77dbd9a3aed78fdd29595
georgiats0 <geotsa001@gmail.com>
2024-01-18 19:14:15 +0200

    m

---
+++ src/main/webapp/js/ajax.js
@@ -0,0 +1,66 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function initDB() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+              $("#ajaxContent").html("Successful Initialization");
+        } else if (xhr.status !== 200) {
+             $("#ajaxContent").html("Error Occured");
+        }
+    };
+
+    xhr.open('GET', 'InitDB');
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send();
+}
+
+function deleteDB() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+              $("#ajaxContent").html("Successful Deletion");
+        } else if (xhr.status !== 200) {
+             $("#ajaxContent").html("Error Occured");
+        }
+    };
+
+    xhr.open('GET', 'DeleteDB');
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send();
+}
+
+
+function createTableFromJSON(data) {
+    var html = "<table><tr><th>Category</th><th>Value</th></tr>";
+    for (const x in data) {
+        var category = x;
+        var value = data[x];
+        html += "<tr><td>" + category + "</td><td>" + value + "</td></tr>";
+    }
+    html += "</table>";
+    return html;
+
+}
+
+
+
+function getUser() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            $("#ajaxContent").html(createTableFromJSON(JSON.parse(xhr.responseText)));
+          //  $("#ajaxContent").html("Successful Login");
+        } else if (xhr.status !== 200) {
+             $("#ajaxContent").html("User not exists or incorrect password");
+        }
+    };
+    var data = $('#loginForm').serialize();
+    xhr.open('GET', 'GetKeeper?'+data);
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send();
+}
