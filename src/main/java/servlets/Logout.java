2c9f9051cb28a5e49d3a68968631e67c5c918d8a
Alexandros <csd5031@csd.uoc.gr>
2024-01-19 20:40:02 +0200

    f

---
+++ src/main/java/servlets/Logout.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loggedIn") != null) {
+            session.invalidate();
+            response.setStatus(200);
+        } else {
+            response.setStatus(403);
+        }
+    }
+
+}
